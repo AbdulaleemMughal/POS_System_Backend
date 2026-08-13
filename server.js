b9f5c514af8aa8d41287078ca51526dfc096c6b1
Abdul Aleem Mughal <137313559+AbdulaleemMughal@users.noreply.github.com>
2026-08-13 17:25:24 +0500

    Change the root file name

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const PORT = process.env.PORT;
 
 const productRoutes = require("./routes/products");
 const orderRoutes = require("./routes/orders");
@@ -15,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-connectDB();
 
 // Routes
 app.use("/api/products", productRoutes);
@@ -27,5 +27,6 @@ app.get("/", (req, res) => {
   res.send("POS API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+});
